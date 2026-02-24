02920ea287f05eff13393267e87d2469a73d146a
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-24 12:01:17 -0300

    feat: header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./header.css";
+
+export function Header() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  function toggleMenu() {
+    setIsOpen(prev => !prev)
+    console.log(isOpen)
+  }
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <img className="logo-image" src="LogoAurora.png" />
+        <h1 className="header-title">Aurora Café</h1>
+      </div>
+
+      <button onClick={toggleMenu} className="menu-btn">
+        ☰
+      </button>
+
+      
+      <nav className={`header-nav ${isOpen ? "open" : ""}`}>
+        <a href="#hero" className="header-nav-item" onClick={toggleMenu}>
+          Início
+        </a>
+        <a href="about" className="header-nav-item" onClick={toggleMenu}>
+          Sobre
+        </a>
+        <a href="menu" className="header-nav-item" onClick={toggleMenu}>
+          Cardápio
+        </a>
+        <a href="contact" className="header-nav-item" onClick={toggleMenu}>
+          Contato
+        </a>
+      </nav>
+    </header>
+  );
+}
