3b80fb3d6f11b443e5b5fff7bfccea0b07f0ebf9
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-24 12:01:07 -0300

    feat: rendering components on main page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Header } from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <header className="app-header">
+        <Header />
+      </header>
+
+      <main className="app-main"></main>
+      <footer className="app-footer"></footer>
+    </>
+  );
+}
+
+export default App;
