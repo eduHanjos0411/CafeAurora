cca8da6edd8d9a0f6d23ab1ecdb7f44e4b83f5b6
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-25 12:27:23 -0300

    feat: testimonial array

---
+++ src/data/Testimonials.ts
@@ -0,0 +1,28 @@
+import type { Testimonial } from "../types/Testimonial";
+
+export const testimonials: Testimonial[] = [
+  {
+    id: 1,
+    name: "Maria Silva",
+    message: "O Café Aurora é o melhor lugar para tomar um café delicioso e relaxar. O ambiente é aconchegante e o atendimento é excelente!",
+    avatarUrl: "mulher1.png",
+  },
+  {
+    id: 2,
+    name: "João Pereira",
+    message: "Adoro os doces do Café Aurora! O bolo de chocolate é simplesmente divino. Recomendo a todos que visitarem a cafeteria.",
+    avatarUrl: "homem1.png",
+  },
+  {
+    id: 3,
+    name: "Ana Costa",
+    message: "O café expresso do Café Aurora é o melhor que já experimentei. Sempre volto para saborear essa delícia!",
+    avatarUrl: "mulher2.png",
+  },
+  {
+    id: 4,
+    name: "Carlos Oliveira",
+    message: "O ambiente do Café Aurora é perfeito para trabalhar ou estudar. A conexão Wi-Fi é rápida e o café é excelente.",
+    avatarUrl: "homem2.png",
+  },
+]
