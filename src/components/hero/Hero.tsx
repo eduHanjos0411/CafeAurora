2ee7951859ffd379c8d40723e8337af3ea108ace
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-25 12:26:52 -0300

    feat: creating hero section

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,14 @@
+import { Button } from '../ui/button/Button'
+import './hero.css'
+
+export function HeroSection() {
+  return (
+    <section id="hero" className="hero-section">
+      <div className='hero-content'>
+        <h2 className='hero-title'>Bem-vindo ao Aurora Café</h2>
+        <h3 className='hero-subtitle'>Seu aconchego diário</h3>
+        <Button>Ver Cardápio</Button>
+      </div>
+    </section>
+  )
+}
