5b35c387f5bd74e6740d8d2a186dbf62d85568af
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-25 12:23:30 -0300

    feat: displaying section components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import "./App.css";
+import { AboutSection } from "./components/about/About";
+import { CardapioSection } from "./components/cardapio/Cardapio";
+import { ContactSection } from "./components/contact/ContactSection";
 import { Header } from "./components/header/Header";
+import { HeroSection } from "./components/hero/Hero";
+import { TestimonialsSection } from "./components/testimonals/Testimonials";
 
 function App() {
   return (
@@ -8,7 +13,13 @@ function App() {
         <Header />
       </header>
 
-      <main className="app-main"></main>
+      <main className="app-main">
+        <HeroSection/>
+        <AboutSection/>
+        <CardapioSection/>
+        <TestimonialsSection/>
+        <ContactSection/>
+      </main>
       <footer className="app-footer"></footer>
     </>
   );
