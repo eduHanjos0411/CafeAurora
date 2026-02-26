cde53b2ffd0d49ccde24217d9dbeca05a92fddf2
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-26 10:35:10 -0300

    feat: contact form

--- src/components/contact/contactForm/ContactForm.tsx
+++ src/components/contact/contactForm/ContactForm.tsx
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
 import { Button } from "../../ui/button/Button";
 import { Input } from "../../ui/input/Input";
 
-import './contactForm.css'
+import "./contactForm.css";
 
-export function ContactForm() {
+interface ContactFormProps {
+  onSubmitForm: (nome: string) => void;
+}
+
+export function ContactForm({ onSubmitForm }: ContactFormProps) {
+  const [nome, setNome] = useState("");
+
+  function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+    onSubmitForm(nome)
+  }
+
   return (
-    <form className="contact-form">
-      <Input  label="Nome" placeholder="Digite seu nome"/>
-      <Input label="Email" placeholder="Digite seu email"/>
-      <Input label="Mensagem" placeholder="Digite sua mensagem"/>
-      <Button>Enviar</Button>
+    <form className="contact-form" onSubmit={handleSubmit}>
+      <Input required value={nome} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNome(e.target.value)} placeholder="Digite seu nome" />
+      <Input required placeholder="Digite seu email" />
+      <Input required placeholder="Digite sua mensagem" />
+      <Button type="submit">Enviar</Button>
     </form>
-  )
+  );
 }
