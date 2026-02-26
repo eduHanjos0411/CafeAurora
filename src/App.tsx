dfe532c94d70c37e6c708894d56cbe364808f5d2
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-26 10:34:23 -0300

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import { AboutSection } from "./components/about/About";
 import { CardapioSection } from "./components/cardapio/Cardapio";
 import { ContactSection } from "./components/contact/ContactSection";
+import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 import { HeroSection } from "./components/hero/Hero";
-import { TestimonialsSection } from "./components/testimonals/Testimonials";
+import { TestimonialsSection } from "./components/testimonals/TestimonialsSection";
 
 function App() {
   return (
@@ -19,8 +20,8 @@ function App() {
         <CardapioSection/>
         <TestimonialsSection/>
         <ContactSection/>
+        <Footer/>
       </main>
-      <footer className="app-footer"></footer>
     </>
   );
 }
