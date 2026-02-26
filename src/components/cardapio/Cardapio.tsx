73900643e191e97ed35e51fd22b3714f736470bf
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-26 10:34:46 -0300

    feat: filter logic working

--- src/components/cardapio/Cardapio.tsx
+++ src/components/cardapio/Cardapio.tsx
@@ -1,20 +1,27 @@
 import { Button } from "../ui/button/Button";
-import { products } from "../../data/Products";
+import { filterProductByCategory } from "../../data/Products";
 import { ProductCard } from "../ui/cards/productCard/ProductCard";
 
 import './cardapio.css'
+import { useState } from "react";
 
 export function CardapioSection() {
+  const [category, setCategory] = useState<string | null>(null)
+
+  const filteredProducts = filterProductByCategory(category)
+
+
   return (
-    <section className="cardapio-section">
+    <section id="menu" className="cardapio-section">
       <h2 className="cardapio-title">Nosso Cardápio</h2>
       <nav className="cardapio-filters">
-        <Button>Bebidas</Button>
-        <Button>Doces</Button>
-        <Button>Salgados</Button>
+        <Button onClick={() => setCategory(null)}>Todos</Button>
+        <Button onClick={() => setCategory("bebida")}>Bebidas</Button>
+        <Button onClick={() => setCategory("doce")}>Doces</Button>
+        <Button onClick={() => setCategory("salgado")}>Salgados</Button>
       </nav>
       <div className="cardapio-itens">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductCard
             id={product.id}
             key={product.id}
